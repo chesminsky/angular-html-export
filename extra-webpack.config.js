34eeddb283399b35338aafe4b6c5dde523258712
chesminsky <chesminsky@gmail.com>
2020-11-11 23:23:12 +0300

    postcss-url

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -2,12 +2,25 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const url = require('postcss-url');
+const urlPlugin = url([{ filter: '**/assets/*.png', url: 'inline' }]);
+
 
 function customizeWebpackConfig(config, _) {
 
 	addHTMLPluginForExports(config);
+	addPostCSSUrlPlugin(config);
 
 	return config;
+}
+
+function addPostCSSUrlPlugin(config) {
+	for (const rule of config.module.rules) {
+		const loader = Array.isArray(rule.use) && rule.use.find(x => x.loader && x.loader.includes('postcss-loader'));
+		if (loader) {
+			loader.options.plugins = [urlPlugin];
+		}
+	}
 }
 
 
